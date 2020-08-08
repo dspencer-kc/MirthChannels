6d3fd9ec06028d7a3c7979aff0ab6ae665bc2134
Drew <drw.spencer@gmail.com>
2020-08-07 23:51:01 -0500

    Fax Data Commit

--- ClientUpdates/SQL/qryClientFaxData.sql
+++ ClientUpdates/SQL/qryClientFaxData.sql
@@ -31,11 +31,37 @@ FROM c_d_client LEFT JOIN c_d_reportchute ON c_d_client.chute_id = c_d_reportchu
    c_d_client.last_updated,
    c_d_client.id,
    c_d_client.enable_chute,
-    (IF c_d_client.enable_chute='Y' THEN c_d_reportchute.phone
-  ELSE ''
-  END), 
+   substr(c_d_reportchute.output_specs, 6, 1) as DistrMethod,
+    CASE
+      WHEN (c_d_client.enable_chute = 'Y' AND 1=1)
+      THEN c_d_reportchute.phone
+    ELSE ''
+    END, 
    c_d_reportchute.phone,
    c_d_reportchute.output_specs
   from c_d_client
   LEFT JOIN c_d_reportchute ON c_d_client.chute_id = c_d_reportchute.id
-  where c_d_client.active_flag = 'A'
+  where c_d_client.active_flag = 'A';
+
+     /*CoPath get all updates since last updated time*/
+  select c_d_client.name,
+   intf_link, 
+   state_id, 
+   city, 
+   street1, 
+   zip, 
+   c_d_client.last_updated,
+   c_d_client.id,
+    CASE
+      WHEN (c_d_client.enable_chute = 'Y' AND RIGHT(LEFT(c_d_reportchute.output_specs, 6),1) ='F')
+      THEN c_d_reportchute.phone
+    ELSE null
+    END as 'Fax1', 
+    CASE
+      WHEN (c_d_client.enable_chute = 'Y' AND RIGHT(LEFT(c_d_reportchute.output_specs, 6),1) ='P')
+      THEN 'Y'
+    ELSE null
+    END as 'Print'
+  from c_d_client
+  LEFT JOIN c_d_reportchute ON c_d_client.chute_id = c_d_reportchute.id
+  where c_d_client.active_flag = 'A';
