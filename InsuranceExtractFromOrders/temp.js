f2a2f03f54ef8673bdbf1ce92c80e40fdf6a9f81
Drew <drw.spencer@gmail.com>
2020-09-29 15:54:29 -0500

    updates

---
+++ InsuranceExtractFromOrders/temp.js
@@ -0,0 +1,6 @@
+UploadToDB(strLName, strFName, strDOB, strSex, strInsuranceName, strClient, strDOS, strOrderID, )
+
+(strLocalLname, strLocalFname, strLocalDOB, strLocalSex, strLocalInsuranceName, strLocalClient, strLocalDOS,strLocalORDERID, strADDRESS, strCITY, strSTATE, strZIP, strGuarantor, strSSN, 
+    strRelationship, strPhone, strInsuranceAddress, strInsuranceCity, strInsuranceState, strInsuranceZip, strInsuranceGroupName, strInsurancePolicyNumber, strSecondaryInsuranceName, 
+    strSecondaryInsuranceAddress, strSecondaryInsuranceCity, strSecondaryInsuranceState, strSecondaryInsuranceZip, strSecondaryInsuranceGroupName, strSecondaryInsurancePolicyNumber, 
+    strSubmittingPhysicianNPI, strSubmittingPhysicianFName, strSubmittingPhysicianLName, strDXCodes, strOrderDate, strInsuranceGroupNumber, strSecondaryInsuranceGroupNumber) 
